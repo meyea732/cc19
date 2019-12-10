9eff5b6430fbe445b9829485a12578e52f79a1e1
meyea732 <54641158+meyea732@users.noreply.github.com>
2019-12-10 17:28:01 -0500

    Add files via upload

--- theperch/theperchscript.js
+++ theperch/theperchscript.js
@@ -1,27 +1,167 @@
-	$(function (){
-var $window = $(window);
-var win_height_padded = $window.height() * 1.1;
-	$window.on('scroll', revealOnScroll);
-	function revealOnScroll() {
-		var scrolled = $window.scrollTop(), win_height_padded = $window.height() * 1.1;
-		$('.revealOnScroll:not(.pile').each(function() {
-			var $this = $(this), offsetTop = $this.offset().top;
-			if (scrolled + win_height_padded > offsetTop) {
-					if ($this.data('timeout')) {
-						window.setTimeout(function () {
-							$this.addClass('pile') + $this.data('pile');
-					}, parseInt($this.data('timeout'), 10));
-				} else {
-					$this.addCLass('pile') +$this.data('pile');
-				}
-			}
-		});
-		$('.revealOnScroll.pile').each(function(index) {
-			var $this = $(this), offsetTop = this.offset().top;
-			if (scrolled + win_height_padded < offsetTop) {
-				$(this) .removeClass('pile fadeInUp flipInX lightSpeedIn');
-			}
-		});
-	}
-	revealOnScroll;
+$(document).ready(function() {
+
+	$("#shoe img").mouseover(function(){
+  	$("#shoe img").css("opacity", "1");
+  	});
+
+	$("#cup img").mouseover(function(){
+  	$("#cup img").css("opacity", "1");
+  	});
+
+  	$("#camera img").mouseover(function(){
+  	$("#camera img").css("opacity", "1");
+  	});
+
+  	$("#prints img").mouseover(function(){
+  	$("#prints img").css("opacity", "1");
+  	});
+
+  	$("#tassle img").mouseover(function(){
+  	$("#tassle img").css("opacity", "1");
+  	});
+
+  	$("#candle img").mouseover(function(){
+  	$("#candle img").css("opacity", "1");
+  	});
+
+  	$("#apple img").mouseover(function(){
+  	$("#apple img").css("opacity", "1");
+  	});
+
+  	$("#key img").mouseover(function(){
+  	$("#key img").css("opacity", "1");
+  	});
+
+  	$("#note img").mouseover(function(){
+  	$("#note img").css("opacity", "1");
+  	});
+
+  	$("#script img").mouseover(function(){
+  	$("#script img").css("opacity", "1");
+  	});
+
+  	$("#film img").mouseover(function(){
+  	$("#film img").css("opacity", "1");
+  	});
+
+  	$("#tape img").mouseover(function(){
+  	$("#tape img").css("opacity", "1");
+  	});
+
+  	$("#pasta img").mouseover(function(){
+  	$("#pasta img").css("opacity", "1");
+  	});
+
+  	$("#mug img").mouseover(function(){
+  	$("#mug img").css("opacity", "1");
+  	});
+
+  	$("#elephant img").mouseover(function(){
+  	$("#elephant img").css("opacity", "1");
+  	});
+
+  	$("#phone img").mouseover(function(){
+  	$("#phone img").css("opacity", "1");
+  	});
+
+  	$("#clock img").mouseover(function(){
+  	$("#clock img").css("opacity", "1");
+  	});
+
+  	$("#cactus img").mouseover(function(){
+  	$("#cactus img").css("opacity", "1");
+  	});
+
+  	$("#rings img").mouseover(function(){
+  	$("#rings img").css("opacity", "1");
+  	});
+
+  	$("#book img").mouseover(function(){
+  	$("#book img").css("opacity", "1");
+  	});
+
+  	$("#shoe2 img").mouseover(function(){
+  	$("#shoe2 img").css("opacity", "1");
+  	});
+
+	$("#cup2 img").mouseover(function(){
+  	$("#cup2 img").css("opacity", "1");
+  	});
+
+  	$("#camera2 img").mouseover(function(){
+  	$("#camera2 img").css("opacity", "1");
+  	});
+
+  	$("#prints2 img").mouseover(function(){
+  	$("#prints2 img").css("opacity", "1");
+  	});
+
+  	$("#tassle2 img").mouseover(function(){
+  	$("#tassle2 img").css("opacity", "1");
+  	});
+
+  	$("#candle2 img").mouseover(function(){
+  	$("#candle2 img").css("opacity", "1");
+  	});
+
+  	$("#apple2 img").mouseover(function(){
+  	$("#apple2 img").css("opacity", "1");
+  	});
+
+  	$("#key2 img").mouseover(function(){
+  	$("#key2 img").css("opacity", "1");
+  	});
+
+  	$("#note2 img").mouseover(function(){
+  	$("#note2 img").css("opacity", "1");
+  	});
+
+  	$("#script2 img").mouseover(function(){
+  	$("#script2 img").css("opacity", "1");
+  	});
+
+  	$("#film2 img").mouseover(function(){
+  	$("#film2 img").css("opacity", "1");
+  	});
+
+  	$("#tape2 img").mouseover(function(){
+  	$("#tape2 img").css("opacity", "1");
+  	});
+
+  	$("#pasta2 img").mouseover(function(){
+  	$("#pasta2 img").css("opacity", "1");
+  	});
+
+  	$("#mug2 img").mouseover(function(){
+  	$("#mug2 img").css("opacity", "1");
+  	});
+
+  	$("#elephant2 img").mouseover(function(){
+  	$("#elephant2 img").css("opacity", "1");
+  	});
+
+  	$("#phone2 img").mouseover(function(){
+  	$("#phone2 img").css("opacity", "1");
+  	});
+
+  	$("#clock2 img").mouseover(function(){
+  	$("#clock2 img").css("opacity", "1");
+  	});
+
+  	$("#cactus2 img").mouseover(function(){
+  	$("#cactus2 img").css("opacity", "1");
+  	});
+
+  	$("#rings2 img").mouseover(function(){
+  	$("#rings2 img").css("opacity", "1");
+  	});
+
+  	$("#book2 img").mouseover(function(){
+  	$("#book2 img").css("opacity", "1");
+  	});
+  	
+  	$("#titlelink").mouseover(function(){
+  	$("#titlelink").css("color", "#F3EC19");
+  	});
+
 });
