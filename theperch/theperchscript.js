1f1015f69bb2cefd15e653bac3283b625739def7
meyea732 <54641158+meyea732@users.noreply.github.com>
2019-12-10 15:28:49 -0500

    Add files via upload

--- theperch/theperchscript.js
+++ theperch/theperchscript.js
@@ -1,29 +1,27 @@
-
-
-// 	$(function (){
-// var $window = $(window);
-// var win_height_padded = $window.height() * 1.1;
-// 	$window.on('scroll', revealOnScroll);
-// 	function revealOnScroll() {
-// 		var scrolled = $window.scrollTop(), win_height_padded = $window.height() * 1.1;
-// 		$('.revealOnScroll:not(.animated').each(function() {
-// 			var $this = $(this), offsetTop = $this.offset().top;
-// 			if (scrolled + win_height_padded > offsetTop) {
-// 					if ($this.data('timeout')) {
-// 						window.setTimeout(function () {
-// 							$this.addClass('animated ') + $this.data('animation');
-// 					}, parseInt($this.data('timeout'), 10));
-// 				} else {
-// 					$this.addCLass('animated ') +$this.data('animation');
-// 				}
-// 			}
-// 		});
-// 		$('.revealOnScroll.animated').each(function(index) {
-// 			var $this = $(this), offsetTop = this.offset().top;
-// 			if (scrolled + win_height_padded < offsetTop) {
-// 				$(this) .removeClass('animated fadeInUp flipInX lightSpeedIn');
-// 			}
-// 		});
-// 	}
-// 	revealOnScroll;
-// });
+	$(function (){
+var $window = $(window);
+var win_height_padded = $window.height() * 1.1;
+	$window.on('scroll', revealOnScroll);
+	function revealOnScroll() {
+		var scrolled = $window.scrollTop(), win_height_padded = $window.height() * 1.1;
+		$('.revealOnScroll:not(.pile').each(function() {
+			var $this = $(this), offsetTop = $this.offset().top;
+			if (scrolled + win_height_padded > offsetTop) {
+					if ($this.data('timeout')) {
+						window.setTimeout(function () {
+							$this.addClass('pile') + $this.data('pile');
+					}, parseInt($this.data('timeout'), 10));
+				} else {
+					$this.addCLass('pile') +$this.data('pile');
+				}
+			}
+		});
+		$('.revealOnScroll.pile').each(function(index) {
+			var $this = $(this), offsetTop = this.offset().top;
+			if (scrolled + win_height_padded < offsetTop) {
+				$(this) .removeClass('pile fadeInUp flipInX lightSpeedIn');
+			}
+		});
+	}
+	revealOnScroll;
+});
